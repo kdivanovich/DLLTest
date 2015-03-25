95786ddfb1280fdebbab98927a0185b0623eb0f0
kdivanovich <gtabro@abv.bg>
2015-03-25 02:44:08 +0000

    DLLTest
    Test SimpleDLL -  a console showing the text should appear.

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "SimpleH.h"	// oh my f***ing god, putting this in angular brackets doesn't work anymore. according to the interwebz this is							done it's not excluded from the build
+#include <iostream>
+using namespace std;
+using namespace nmspace;
+
+int main()
+{
+	myclass::Crap();
+	system("PAUSE");
+}
